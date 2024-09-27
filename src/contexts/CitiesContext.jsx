159d8fb43d819fe37866357a87320980e2468b3e
besher al addam <beshoux@outlook.fr>
2024-09-27 17:11:53 +0200

    Refactor CitiesContext to use useReducer for state management

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,81 +1,129 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 
 const citiesContext = createContext();
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "loading":
+      return { ...state, isLoading: true };
+
+    case "cities/loaded":
+      return { ...state, isLoading: false, cities: action.payload };
+
+    case "city/added":
+      return {
+        ...state,
+        isLoading: false,
+        cities: [...state.cities, action.payload],
+        currentCity: action.payload,
+      };
+
+    case "city/deleted":
+      return {
+        ...state,
+        isLoading: false,
+        cities: state.cities.filter((city) => city.id !== action.payload),
+        currentCity: {},
+      };
+
+    case "city/loaded":
+      return { ...state, isLoading: false, currentCity: action.payload };
+
+    case "rejected":
+      return { ...state, isLoading: false, error: action.payload };
+
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 function CitiesProvider({ children }) {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({});
+  const initialState = {
+    cities: [],
+    isLoading: false,
+    currentCity: {},
+    error: "",
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { cities, isLoading, currentCity, error } = state;
 
   useEffect(() => {
     async function fetchCities() {
+      dispatch({ type: "loading" });
       try {
-        setIsLoading(true);
         const sotredCities = localStorage.getItem("cities");
+
         if (sotredCities) {
-          setCities(JSON.parse(sotredCities));
+          dispatch({
+            type: "cities/loaded",
+            payload: JSON.parse(sotredCities),
+          });
         } else {
           const res = await fetch(
             "https://akai54.github.io/worldwise/data/cities.json"
           );
           const data = await res.json();
 
-          setCities(data.cities);
+          dispatch({ type: "cities/loaded", payload: data });
         }
       } catch (error) {
-        console.error(error);
-      } finally {
-        setIsLoading(false);
+        dispatch({ type: "rejected", payload: error.message });
       }
     }
     fetchCities();
   }, []);
 
+  /* Save cities to local storage */
   useEffect(() => {
     localStorage.setItem("cities", JSON.stringify(cities));
   }, [cities]);
 
   function getCity(id) {
+    if (Number(id) === currentCity.id) return;
+
+    dispatch({ type: "loading" });
     try {
-      setIsLoading(true);
       const city = cities.find((city) => city.id == id);
       if (!city) {
         throw new Error(`City with id ${id} not found`);
       }
-      setCurrentCity(city);
+      dispatch({ type: "city/loaded", payload: city });
     } catch (error) {
-      console.error(error);
-    } finally {
-      setTimeout(() => setIsLoading(false), 500);
+      dispatch({ type: "rejected", payload: error.message });
     }
   }
 
   function addCity(city) {
+    dispatch({ type: "loading" });
     try {
-      setIsLoading(true);
-      setCities([...cities, city]);
+      dispatch({ type: "city/added", payload: city });
     } catch (error) {
-      console.error(error);
-    } finally {
-      setTimeout(() => setIsLoading(false), 500);
+      dispatch({ type: "rejected", payload: error.message });
     }
   }
 
   function deleteCity(city) {
+    dispatch({ type: "loading" });
     try {
-      setIsLoading(true);
-      const newCities = cities.filter((c) => c.id !== city.id);
-      setCities(newCities);
+      dispatch({ type: "city/deleted", payload: city.id });
     } catch (error) {
-      console.error(error);
-    } finally {
-      setTimeout(() => setIsLoading(false), 500);
+      dispatch({ type: "rejected", payload: error.message });
     }
   }
 
   return (
     <citiesContext.Provider
-      value={{ cities, isLoading, currentCity, getCity, addCity, deleteCity }}
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+        addCity,
+        deleteCity,
+        error,
+      }}
     >
       {children}
     </citiesContext.Provider>
